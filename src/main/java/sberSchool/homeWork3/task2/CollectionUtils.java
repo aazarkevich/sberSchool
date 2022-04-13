0a5b95194b4de585d5879d759eb4e21f61cd607b
aazarkevich <artem.azarckevich@yandex.ru>
2022-04-13 21:43:00 +0300

    homework 3 task 2

--- src/main/java/sberSchool/homeWork3/task2/CollectionUtils.java
+++ src/main/java/sberSchool/homeWork3/task2/CollectionUtils.java
@@ -1,6 +1,7 @@
 package sberSchool.homeWork3.task2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CollectionUtils {
@@ -9,12 +10,50 @@ public class CollectionUtils {
         destination.addAll(source);
     }
 
-    public static <T> List newArrayList() {
-        return new ArrayList<T>();
+    public static <T> List<T> newArrayList() {
+        return new ArrayList();
     }
 
-    public static <T> List limit(List<? extends T> source, int size) {
-//        return newArrayList(source.subList(0,size));
-        return null;
+    public static <T> int indexOf(List<? extends T> source, Object o) {
+        return source.indexOf(o);
     }
+
+    public static <T> List<T> limit(List<? extends T> source, int size) {
+        return new ArrayList(source.subList(0,size));
+    }
+
+
+    public static <T> void add(List<? super T> source, T o) {
+        source.add(o);
+    }
+
+    public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> c2) {
+        c2.forEach(v -> removeFrom.remove(v));
+
+    }
+
+    public static <T> boolean containsAll(List<? extends T> c1, List<T> c2) {
+        return c1.containsAll(c2);
+    }
+
+    public static <T> boolean containsAny(List<? super T> c1, List<? extends T> c2) {
+        for (T elem: c2) {
+            if(c1.contains(elem)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static <T extends Comparable<? super T>> List<T> range(List<? extends T> list, Object min, Object max) {
+        List<T> result = new ArrayList<>();
+        for (T elem : list) {
+            if (elem.compareTo((T) min) >= 0 && elem.compareTo((T) max) <= 0) {
+                result.add(elem);
+            }
+        }
+        Collections.sort(result);
+        return result;
+    }
+
 }
--- src/main/java/sberSchool/homeWork3/task2/Main.java
+++ src/main/java/sberSchool/homeWork3/task2/Main.java
@@ -7,10 +7,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> testik = new ArrayList<>(Arrays.asList(1,2,34,5,6));
+        List testik = new ArrayList<>(Arrays.asList(1,2,34,5,6));
+        List<Number> testik2 = new ArrayList<>(Arrays.asList(1,2,34,5,6));
 //        List<Integer> testik2 = new ArrayList<>();
 //        CollectionUtils.addAll(testik,testik2);
+//        List list = CollectionUtils.limit(testik, 3);
+//        list.forEach(System.out::println);
+//        System.out.println(CollectionUtils.indexOf(testik, 2));
+//        CollectionUtils.add(testik,24);
 //        testik.forEach(System.out::println);
+//        CollectionUtils.removeAll(testik2,testik);
+//        testik2.forEach(System.out::println);
+
+//        System.out.println(CollectionUtils.containsAll(testik,testik2));
+//        System.out.println(CollectionUtils.containsAny(testik, testik2));
+
+        Integer.compare(1,5);
     }
 
     static void test(List<? super Integer> testList) {
