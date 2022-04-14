d3370d267a55fdcf8f38ae1800e2992cb3ca85e1
aazarkevich <artem.azarckevich@yandex.ru>
2022-04-14 08:54:19 +0300

    homework3

--- src/main/java/sberSchool/homeWork3/task1/CountMap.java
+++ src/main/java/sberSchool/homeWork3/task1/CountMap.java
@@ -3,18 +3,26 @@ package sberSchool.homeWork3.task1;
 import java.util.Map;
 
 public interface CountMap<T> {
+    // добавляет элемент в этот контейнер.
     void add(T element);
 
+    //Возвращает количество добавлений данного элемента
     int getCount(T element);
 
-    //current count
+    //Удаляет элемент и контейнера и возвращает количество его добавлений(до удаления)
     int remove(T element);
 
+    //количество разных элементов
     int size();
 
+    //Добавить все элементы из source в текущий контейнер,
+    // при совпадении ключей,     суммировать значения
     void addAll(CountMap source);
 
+    //Вернуть java.util.Map. ключ - добавленный элемент,
+    // значение - количество его добавлений
     Map toMap();
 
+    //Тот же самый контракт как и toMap(), только всю информацию записать в destination
     void toMap(Map destination);
 }
--- src/main/java/sberSchool/homeWork3/task2/Main.java
+++ src/main/java/sberSchool/homeWork3/task2/Main.java
@@ -7,25 +7,47 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List testik = new ArrayList<>(Arrays.asList(1,2,34,5,6));
-        List<Number> testik2 = new ArrayList<>(Arrays.asList(1,2,34,5,6));
-//        List<Integer> testik2 = new ArrayList<>();
-//        CollectionUtils.addAll(testik,testik2);
-//        List list = CollectionUtils.limit(testik, 3);
-//        list.forEach(System.out::println);
-//        System.out.println(CollectionUtils.indexOf(testik, 2));
-//        CollectionUtils.add(testik,24);
-//        testik.forEach(System.out::println);
-//        CollectionUtils.removeAll(testik2,testik);
-//        testik2.forEach(System.out::println);
+        List list1 = new ArrayList<>(Arrays.asList(1,2,34,5,6));
+        List<Number> list2 = new ArrayList<>();
 
-//        System.out.println(CollectionUtils.containsAll(testik,testik2));
-        System.out.println(CollectionUtils.containsAny(testik, testik2));
+        System.out.println("CollectionUtils.addAll()");
+        CollectionUtils.addAll(list1, list2);
+        list2.forEach(System.out::println);
+        System.out.println("----------");
 
+        System.out.println("CollectionUtils.newArraylist()");
+        List<Integer> newList = new ArrayList<>();
+
+        System.out.println("CollectionUtils.indexOf()");
+        System.out.println(CollectionUtils.indexOf(list1, 2));
+        System.out.println("----------");
+
+        System.out.println("CollectionUtils.limit()");
+        List subList = CollectionUtils.limit(list1,3);
+        subList.forEach(System.out::println);
+        System.out.println("----------");
+
+        System.out.println("CollectionUtils.add()");
+        CollectionUtils.add(list1, 55);
+        list1.forEach(System.out::println);
+        System.out.println("----------");
+
+        System.out.println("CollectionUtils.removeAll()");
+        CollectionUtils.removeAll(list2, Arrays.asList(1,2,34));
+        list2.forEach(System.out::println);
+        System.out.println("----------");
+
+        System.out.println("CollectionUtils.containsAll()");
+        System.out.println(CollectionUtils.containsAll(list1,list2));
+        System.out.println("----------");
+
+        System.out.println("CollectionUtils.containsAny()");
+        System.out.println(CollectionUtils.containsAny(list1, list2));
+        System.out.println("----------");
+
+        System.out.println("CollectionUtils.range()");
+        System.out.println(CollectionUtils.range(list1,1,6));
+        System.out.println("----------");
     }
 
-    static void test(List<? super Integer> testList) {
-        System.out.println(testList.add(0));
-//        testList.forEach(System.out::println);
-    }
 }
